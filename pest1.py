88c81570afcdd4c49692999d3b5844ebcdc5eb33
Hayanna Castro <152522247+hayhay07@users.noreply.github.com>
2024-10-08 10:42:49 -0300

    Create pest1.py
    cadastro

---
+++ pest1.py
@@ -0,0 +1,10 @@
+#cadastro
+nome = input("Digite seu nome completo: ")
+idade = int(input("Digite sua idade: "))
+peso = float(input("Digite seu peso : "))
+altura = float(input("Digite sua altura: "))
+
+print(f'Nome: {nome}')
+print(f'Idade: {idade}')
+print(f'Peso: {peso}')
+print(f'Altura: {altura}')
